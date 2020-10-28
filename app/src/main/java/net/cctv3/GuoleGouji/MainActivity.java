817650ceffed058ccccd91af900c5d94f8e143a6
ChenqiaoYizhan <i@cctv3.net>
2020-10-28 23:26:44 +0800

    1.初步完成OpenCV集成 -_-||

--- app/src/main/java/net/cctv3/GuoleGouji/MainActivity.java
+++ app/src/main/java/net/cctv3/GuoleGouji/MainActivity.java
@@ -2,12 +2,38 @@ package net.cctv3.GuoleGouji;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 测试OpenCV配置
+        if (!OpenCVLoader.initDebug()) {
+            Log.d("OpenCV", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, baseLoaderCallback);
+        } else {
+            Log.d("OpenCV", "OpenCV library found inside package. Using it!");
+            baseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
     }
+
+    private BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    Toast.makeText(MainActivity.this, "OpenCV config: Ok", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    Toast.makeText(MainActivity.this, "OpenCV config: Error", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 }
